16ad3fe1d676c9c7b5e6967d1a631610cbe14ec0
Nguyenphu8223 <125480866+Nguyenphu8223@users.noreply.github.com>
2023-03-07 12:19:58 +0700

    Update bai4.java

--- bai4.java
+++ bai4.java
@@ -1,19 +1,1 @@
-package bai1;
 
-import java.util.Scanner;
-
-public class bai4 {
-	public static void main(String[] args)
-	{
-		int a;
-		try (Scanner sc = new Scanner(System.in)) {
-			System.out.println("nhap so tu 1 den 12: ");
-			a=sc.nextInt();
-		}
-
-		if (a>=1 & a<=12)
-			System.out.println("thang "+a);
-		else
-			System.out.println("nhap sai");
-	}
-}
