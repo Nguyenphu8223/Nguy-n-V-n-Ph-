b480884f0257a47b5cbc9d45375927eddff92d08
Nguyenphu8223 <125480866+Nguyenphu8223@users.noreply.github.com>
2023-03-07 12:19:29 +0700

    Update bai2.java

--- bai2.java
+++ bai2.java
@@ -1,18 +1,1 @@
-package bai1;
 
-import java.util.Scanner;
-
-public class bai2 {
-	public static void main(String[] args)
-	{
-		int a;
-		try (Scanner sc = new Scanner(System.in)) {
-			System.out.println("nhap so nguyen a: ");
-			a=sc.nextInt();
-		} 
-		if (a % 2==0)
-			System.out.println("so vua nhap la so chan");
-		else
-			System.out.println("so vua nhap la so le");
-	}
-}
